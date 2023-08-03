7d5d8015d134e30107777654982b7533d92fe169
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-08-02 23:59:59 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_collapsing_toolbar/flutter_collapsing_toolbar.dart';
 import 'package:lottie/lottie.dart';
-import 'dados.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 import 'package:url_launcher/url_launcher.dart';
-
-void main() => runApp(MyApp());
+import 'package:flutter/services.dart';
 
 const kSampleIcons = [
   Icons.track_changes_outlined,
@@ -16,23 +16,22 @@ const kSampleIconLabels = [
   'GITHUB',
 ];
 
-class MyApp extends StatefulWidget {
+class dados extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _dadosState createState() => _dadosState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _dadosState extends State<dados> {
   final controller = ScrollController();
   double headerOffset = 0.0;
   final String githubUrl = 'https://github.com/LeticiaBHB';
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
       routes: {
         '/dados': (context) => dados(),
+        '/dadospage': (context) => DadosPage(),
       },
       home: Scaffold(
         body: Container(
@@ -123,28 +122,39 @@ class _MyAppState extends State<MyApp> {
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Text(
-                                  "PM2.5 é um sério problema de poluição atmosférica que afeta a saúde e o meio ambiente.",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 20),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(context, MaterialPageRoute(builder: (context) => DadosPage()));
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.orange[200], // Define a cor de fundo do botão
+                                    onPrimary: Colors.black, // Define a cor do texto do botão
+                                  ),
+                                  child: Text(
+                                    'Aqui estão os dados compilados na linguagem DART',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.black, fontSize: 20),
+                                  ),
                                 ),
                                 SizedBox(height: 16),
-                                // Espaço de 16 de altura entre os parágrafos
-                                Text(
-                                  "E a medicina veterinária é a guardiã da Saúde Única. Tendo isto em vista venho por meio deste apresentar os dados processados, em comparação com mortalidade de aves migratórias",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 20),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Lógica a ser executada ao pressionar o botão 2
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.orange[200],
+                                    onPrimary: Colors.black,
+                                  ),
+                                  child: Text(
+                                    'Aqui estão os dados compilados na linguagem PYTHON',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 20),
+                                  ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        Container(
-                          child: Image.asset('assets/saudeunica.png'),
-                          width: 200,
-                          height: 200,
                         ),
                       ],
                     ),
@@ -157,8 +167,7 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-
-  // Método para abrir URLs em um navegador externo
+// Método para abrir URLs em um navegador externo
   Future<void> _launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
@@ -171,14 +180,93 @@ class _MyAppState extends State<MyApp> {
   void _onFeaturePressed(BuildContext context, int index) {
     switch (index) {
       case 0:
-        // Abre a página 'dados' quando o primeiro recurso é pressionado
+      // Abre a página 'dados' quando o primeiro recurso é pressionado
         Navigator.pushNamed(context, '/dados');
         break;
       case 1:
-        // Abre o link do GitHub no navegador externo quando o segundo recurso é pressionado
+      // Abre o link do GitHub no navegador externo quando o segundo recurso é pressionado
         _launchURL(githubUrl);
         break;
       default:
     }
   }
 }
+////////////////////////////////////////////////////////////////////////////////
+//PRIMEIRO BOTÃO
+
+void main() {
+  runApp(MaterialApp(
+    home: DadosPage(),
+  ));
+}
+
+class DadosPage extends StatefulWidget {
+  @override
+  _DadosPageState createState() => _DadosPageState();
+}
+
+class _DadosPageState extends State<DadosPage> {
+  List<List<dynamic>> csvTable = [];
+  bool isCalculating = true;
+
+  @override
+  void initState() {
+    super.initState();
+    readCsvData();
+  }
+
+  Future<void> readCsvData() async {
+    try {
+      // Ler o arquivo CSV 'dadospm25.csv' como bytes
+      ByteData data = await rootBundle.load('assets/dadospm25.csv');
+      List<int> bytes = data.buffer.asUint8List();
+
+      // Converter os bytes para uma string
+      String csvData = String.fromCharCodes(bytes);
+
+      // Converter o conteúdo do CSV em uma lista de listas de dynamic
+      csvTable = CsvToListConverter(fieldDelimiter: '|').convert(csvData);
+
+      setState(() {
+        isCalculating = false;
+      });
+    } catch (e) {
+      print("Erro ao ler o arquivo CSV: $e");
+      setState(() {
+        isCalculating = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dados CSV em Tabela'),
+      ),
+      body: Center(
+        child: isCalculating
+            ? CircularProgressIndicator()
+            : SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: DataTable(
+              columns: csvTable[0].map<DataColumn>((column) {
+                return DataColumn(label: Text(column.toString()));
+              }).toList(),
+              rows: csvTable.sublist(1).map<DataRow>((row) {
+                return DataRow(
+                  cells: row.map<DataCell>((cell) {
+                    return DataCell(Text(cell.toString()));
+                  }).toList(),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
